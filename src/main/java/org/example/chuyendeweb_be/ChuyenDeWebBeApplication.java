0bff33ad875fd4bce17f0198fbe048475aed92f2
ThanhTan6723 <21130528@st.hcmuaf.edu.vn>
2025-04-29 19:39:59 +0700

    first commit

---
+++ src/main/java/org/example/chuyendeweb_be/ChuyenDeWebBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.chuyendeweb_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuyenDeWebBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuyenDeWebBeApplication.class, args);
+	}
+
+}
