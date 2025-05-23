192abe8b29d4b5381434a5815d9c660751169cf2
NguyenQuocTrungg <21130588@st.hcmuaf.edu.vn>
2025-05-23 22:34:40 +0700

    Forgot password

---
+++ src/main/java/org/example/chuyendeweb_be/dto/ForgotPasswordRequest.java
@@ -0,0 +1,22 @@
+package org.example.chuyendeweb_be.dto;
+
+public class ForgotPasswordRequest {
+    private String email;
+    private String frontendUrl;
+
+    public ForgotPasswordRequest() {}
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFrontendUrl() {
+        return frontendUrl;
+    }
+    public void setFrontendUrl(String frontendUrl) {
+        this.frontendUrl = frontendUrl;
+    }
+}
